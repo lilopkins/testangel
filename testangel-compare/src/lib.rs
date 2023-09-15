ca5aae9f52f00d6fe33831b1dedab6a2462b3948
Lily Hopkins <lily@hpkns.uk>
2023-09-15 16:12:43 +0100

    feat: added logic operations
    resolves #83

--- testangel-compare/src/lib.rs
+++ testangel-compare/src/lib.rs
@@ -43,6 +43,24 @@ lazy_static! {
         })
     .with_instruction(
         Instruction::new(
+            "compare-eq-bool",
+            "Equal (Boolean)",
+            "Compare the value of two Booleans.",
+        )
+        .with_parameter("val1", "A", ParameterKind::Boolean)
+        .with_parameter("val2", "B", ParameterKind::Boolean)
+        .with_output("result", "A = B", ParameterKind::Boolean),
+        |_state, params, output, _evidence| {
+            let val1 = params["val1"].value_bool();
+            let val2 = params["val2"].value_bool();
+
+            // Produce output and evidence
+            let result = val1 == val2;
+            output.insert("result".to_owned(), ParameterValue::Boolean(result));
+            None
+        })
+    .with_instruction(
+        Instruction::new(
             "compare-not",
             "Not (Boolean)",
             "If fed true, returns false, if fed false, returns true.",
@@ -54,6 +72,42 @@ lazy_static! {
 
             // Produce output and evidence
             let result = !val1;
+            output.insert("result".to_owned(), ParameterValue::Boolean(result));
+            None
+        })
+    .with_instruction(
+        Instruction::new(
+            "compare-and",
+            "And (Boolean)",
+            "Returns true if both A and B are true.",
+        )
+        .with_parameter("val1", "A", ParameterKind::Boolean)
+        .with_parameter("val2", "B", ParameterKind::Boolean)
+        .with_output("result", "A and B", ParameterKind::Boolean),
+        |_state, params, output, _evidence| {
+            let val1 = params["val1"].value_bool();
+            let val2 = params["val2"].value_bool();
+
+            // Produce output and evidence
+            let result = val1 && val2;
+            output.insert("result".to_owned(), ParameterValue::Boolean(result));
+            None
+        })
+    .with_instruction(
+        Instruction::new(
+            "compare-or",
+            "Or (Boolean)",
+            "Returns true if A or B is true.",
+        )
+        .with_parameter("val1", "A", ParameterKind::Boolean)
+        .with_parameter("val2", "B", ParameterKind::Boolean)
+        .with_output("result", "not A", ParameterKind::Boolean),
+        |_state, params, output, _evidence| {
+            let val1 = params["val1"].value_bool();
+            let val2 = params["val2"].value_bool();
+
+            // Produce output and evidence
+            let result = val1 || val2;
             output.insert("result".to_owned(), ParameterValue::Boolean(result));
             None
         })
