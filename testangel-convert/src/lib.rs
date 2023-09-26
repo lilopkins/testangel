346254f2d63f20d204d9bc5be6d53a91d8208039
Lily Hopkins <lily@hpkns.uk>
2023-09-26 12:03:06 +0100

    fix: fixed concat being wrong types

--- testangel-convert/src/lib.rs
+++ testangel-convert/src/lib.rs
@@ -43,8 +43,8 @@ lazy_static! {
             "Concatenate Strings",
             "Concatenate two strings into one.",
         )
-        .with_parameter("val1", "StringA", ParameterKind::Decimal)
-        .with_parameter("val2", "StringB", ParameterKind::Decimal)
+        .with_parameter("val1", "StringA", ParameterKind::String)
+        .with_parameter("val2", "StringB", ParameterKind::String)
         .with_output("result", "StringAStringB", ParameterKind::String),
         |_state, params, output, _evidence| {
             let val1 = params["val1"].value_string();
