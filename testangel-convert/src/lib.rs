3ca5e267842eba33fd30e59d7094fe9c7b06de30
Lily Hopkins <lily@hpkns.uk>
2023-12-08 11:47:31 +0000

    feat: added interpolation
    resolves #99

--- testangel-convert/src/lib.rs
+++ testangel-convert/src/lib.rs
@@ -1,10 +1,16 @@
-use std::sync::Mutex;
+use std::{collections::HashMap, sync::Mutex};
 
+use interpolator::{format, Formattable};
 use lazy_static::lazy_static;
 use testangel_engine::*;
 
+#[derive(Default)]
+struct State {
+    interpolation_values: HashMap<String, String>,
+}
+
 lazy_static! {
-    static ref ENGINE: Mutex<Engine<'static, ()>> = Mutex::new(Engine::new("Convert", env!("CARGO_PKG_VERSION"))
+    static ref ENGINE: Mutex<Engine<'static, State>> = Mutex::new(Engine::new("Convert", env!("CARGO_PKG_VERSION"))
     .with_instruction(
         Instruction::new(
             "convert-int-string",
@@ -52,6 +58,42 @@ lazy_static! {
 
             // Produce output and evidence
             let result = format!("{val1}{val2}");
+            output.insert("result".to_owned(), ParameterValue::String(result));
+            Ok(())
+        })
+    .with_instruction(
+        Instruction::new(
+            "convert-add-interpolate-value",
+            "Interpolate Strings: Set Value",
+            "Set a value to use during future string interpolation.",
+        )
+        .with_parameter("key", "Key", ParameterKind::String)
+        .with_parameter("value", "Value", ParameterKind::String),
+        |state: &mut State, params, _output, _evidence| {
+            let key = params["key"].value_string();
+            let value = params["value"].value_string();
+
+            state.interpolation_values.insert(key, value);
+            Ok(())
+        })
+    .with_instruction(
+        Instruction::new(
+            "convert-interpolate-strings",
+            "Interpolate Strings",
+            "Interpolate values into a string. Uses values set previously by 'Interpolate Strings: Set Value'. For formatting guidance, see https://hpkns.uk/tafmt.",
+        )
+        .with_parameter("template", "Template String", ParameterKind::String)
+        .with_output("result", "Interpolated String", ParameterKind::String),
+        |state, params, output, _evidence| {
+            let template = params["template"].value_string();
+
+            let mut values: HashMap<&str, Formattable<'_>> = HashMap::new();
+            for (k, v) in &state.interpolation_values {
+                values.insert(k, Formattable::display(v));
+            }
+
+            // Produce output and evidence
+            let result = format(&template, &values)?;
             output.insert("result".to_owned(), ParameterValue::String(result));
             Ok(())
         })
