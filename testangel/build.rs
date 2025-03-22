62cf8fcca0ee2f2ae997cd2dd7d603ff5958705f
Lily Hopkins <lily@hpkns.uk>
2025-03-22 17:43:22 +0000

    fix: tweaked resources for windows
    fixes #257

--- testangel/build.rs
+++ testangel/build.rs
@@ -1,16 +1,17 @@
-#[cfg(windows)]
 fn main() {
-    println!("cargo::rerun-if-changed=../icon.png");
+    if cfg!(feature = "cli") || cfg!(feature = "ui") {
+        println!("cargo::rerun-if-changed=../icon.png");
 
-    ico_builder::IcoBuilder::default()
-        .add_source_file("../icon.png")
-        .build_file("../icon.ico")
-        .unwrap();
+        #[cfg(windows)]
+        {
+            ico_builder::IcoBuilder::default()
+                .add_source_file("../icon.png")
+                .build_file("../icon.ico")
+                .unwrap();
 
-    let mut res = winres::WindowsResource::new();
-    res.set_icon("../icon.ico");
-    res.compile().unwrap();
+            let mut res = winres::WindowsResource::new();
+            res.set_icon("../icon.ico");
+            res.compile().unwrap();
+        }
+    }
 }
-
-#[cfg(not(windows))]
-fn main() {}
